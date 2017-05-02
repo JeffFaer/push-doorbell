1a31e2aaf70afab06f9ce103e4d093983050012c
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2017-05-02 17:39:00 -0600

    Add the ability to customize touch thresholds.

--- touch.js
+++ touch.js
@@ -14,20 +14,23 @@ var options = {
 
 var lastMessage = null;
 var mpr121 = new MPR121(0x5A, 1);
-mpr121.on('touch', function(pin) {
-    debug(`Touch on ${pin}`);
+mpr121.setThresholds(config.thresholdTouch, config.thresholdRelease)
+    .then(function() {
+        mpr121.on('touch', function(pin) {
+            debug(`Touch on ${pin}`);
 
-    var currentTime = new Date().getTime();
-    if (lastMessage == null
-            || currentTime - lastMessage > config.notificationWait) {
-        debug('Sending notification.');
-        lastMessage = currentTime;
-        messaging.sendToDevice(tokens.getTokens(), payload, options);
-    } else {
-        var timeUntilMessage = config.notificationWait -
-            (currentTime - lastMessage);
-        debug(`${timeUntilMessage} ms until next notification`);
-    }
-});
+            var currentTime = new Date().getTime();
+            if (lastMessage == null
+                    || currentTime - lastMessage > config.notificationWait) {
+                debug('Sending notification.');
+                lastMessage = currentTime;
+                messaging.sendToDevice(tokens.getTokens(), payload, options);
+            } else {
+                var timeUntilMessage = config.notificationWait -
+                    (currentTime - lastMessage);
+                debug(`${timeUntilMessage} ms until next notification`);
+            }
+        });
+    });
 
 module.exports = mpr121;
